2aa6b930f4ad4bba27f35d04c551aaada12bdaf4
Igor Soroka <stupbird@gmail.com>
2019-07-05 21:45:47 +0300

    added new endpoint for searching by coordinates

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,21 +15,40 @@ const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 const port = process.env.PORT || 9000;
 
 const app = new Koa();
+const BASE_URL = `/api`;
 
 app.use(cors());
 
-const fetchWeather = async () => {
+const fetchWeatherByCityName = async () => {
   const endpoint = `${mapURI}/weather?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
-
   return response ? response.json() : {};
 };
 
-router.get('/api/weather', async ctx => {
-  const weatherData = await fetchWeather();
+const fetchWeatherByGeoCoordinates = async (lat, long) => {
+  const geoEndpoint = `${mapURI}/weather?lat=${lat}&lon=${long}&appid=${appId}`;
+  const geoResponse = await fetch(geoEndpoint);
+  return geoResponse ? geoResponse.json() : {};
+};
 
+router.get(`${BASE_URL}/weather`, async ctx => {
+  const weatherData = await fetchWeatherByCityName();
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+});
+
+router.get(`${BASE_URL}/weather/:coords`, async ctx => {
+  const coordsArray = ctx.params.coords.split(',');
+  const weatherData = await fetchWeatherByGeoCoordinates(coordsArray[0], coordsArray[1]);
+  if (weatherData && weatherData.weather[0]) {
+    ctx.body = {
+      weather: weatherData.weather[0],
+      country: weatherData.sys.country,
+      name: weatherData.name,
+    };
+  } else {
+    ctx.body = {};
+  }
 });
 
 app.use(router.routes());
