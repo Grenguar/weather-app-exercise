068235e6b5cef73247cf5a29af1a23ef0f30b955
Igor Soroka <stupbird@gmail.com>
2019-07-08 21:14:08 +0300

    final changes, I believe

--- prepare_env.sh
+++ prepare_env.sh
@@ -7,17 +7,10 @@ gcloud builds submit --tag gcr.io/$GCLOUD_PROJECT/weatherapp_backend ./backend/
 
 echo "Deploying to Container Engine"
 export GCLOUD_PROJECT=$DEVSHELL_PROJECT_ID
-export APPID=$WEATHER_API
-export GEO_API_KEY=$GEO_API
 sed -i -e "s/\[GCLOUD_PROJECT\]/$GCLOUD_PROJECT/g" ./backend/backend-deployment.yml
 sed -i -e "s/\[APPID\]/$APPID/g" ./backend/backend-deployment.yml
 sed -i -e "s/\[GCLOUD_PROJECT\]/$GCLOUD_PROJECT/g" ./frontend/frontend-deployment.yml
 sed -i -e "s/\[GEO_API_KEY\]/$GEO_API_KEY/g" ./frontend/frontend-deployment.yml
-echo "###### backend-deployment ######"
-cat ./backend/backend-deployment.yml
-echo "###### frontend-deployment ######"
-cat ./frontend/frontend-deployment.yml
-echo "######"
 kubectl create -f ./backend/backend-deployment.yml
 kubectl create -f ./backend/backend-service.yml
 kubectl create -f ./frontend/frontend-deployment.yml
