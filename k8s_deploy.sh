84602dd89a4dcdb6f44253454dad4b316b077eb0
Igor Soroka <stupbird@gmail.com>
2019-07-11 17:14:39 +0300

    small changes in k8s script

--- k8s_deploy.sh
+++ k8s_deploy.sh
@@ -3,23 +3,21 @@ export GCLOUD_PROJECT=$DEVSHELL_PROJECT_ID
 gcloud container clusters create weatherapp-cluster --zone europe-north1-a --scopes cloud-platform
 gcloud container clusters get-credentials weatherapp-cluster --zone europe-north1-a
 
-gcloud builds submit --tag gcr.io/$GCLOUD_PROJECT/weatherapp_frontend ./frontend/
 gcloud builds submit --tag gcr.io/$GCLOUD_PROJECT/weatherapp_backend ./backend/
-
-echo "Setting up environment variables"
-sed -i -e "s/\[GCLOUD_PROJECT\]/$GCLOUD_PROJECT/g" ./backend/backend-deployment.yml
-sed -i -e "s/\[APPID\]/$APPID/g" ./backend/backend-deployment.yml
-sed -i -e "s/\[GCLOUD_PROJECT\]/$GCLOUD_PROJECT/g" ./frontend/frontend-deployment.yml
-sed -i -e "s/\[GEO_API_KEY\]/$GEO_API_KEY/g" ./frontend/frontend-deployment.yml
+gcloud builds submit --tag gcr.io/$GCLOUD_PROJECT/weatherapp_frontend ./frontend/
 
 echo "Deploying to Container Engine"
+sed -i -e "s/\[GCLOUD_PROJECT\]/$GCLOUD_PROJECT/g" ./backend/backend-deployment.yml
+sed -i -e "s/\[APPID\]/$APPID/g" ./backend/backend-deployment.yml   
 kubectl create -f ./backend/backend-deployment.yml
 kubectl create -f ./backend/backend-service.yml
 ip=""; while [ -z $ip ]; do echo "Waiting for end point..."; ip=$(kubectl get svc weatherapp-backend-service --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}"); [ -z "$ip" ] && sleep 10; done; echo "End point:" && echo $ip; export ENDPOINT=$ip
+sed -i -e "s/\[GCLOUD_PROJECT\]/$GCLOUD_PROJECT/g" ./frontend/frontend-deployment.yml
+sed -i -e "s/\[GEO_API_KEY\]/$GEO_API_KEY/g" ./frontend/frontend-deployment.yml
 sed -i -e "s/\[ENDPOINT\]/$ENDPOINT/g" ./frontend/frontend-deployment.yml
 kubectl create -f ./frontend/frontend-deployment.yml
 kubectl create -f ./frontend/frontend-service.yml
-ip=""; while [ -z $ip ]; do echo "Waiting for frontend end point..."; ip=$(kubectl get svc weatherapp-frontend --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}"); [ -z "$ip" ] && sleep 10; done; echo "Frontend ip:" && echo $ip; export FRONTEND=$ip
+ip2=""; while [ -z $ip2 ]; do echo "Waiting for frontend end point..."; ip2=$(kubectl get svc weatherapp-frontend --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}"); [ -z "$ip2" ] && sleep 10; done; echo "Frontend ip:" && echo $ip2; export FRONTEND=$ip2
 
 
 echo "Backend API (for Helsinki): http://$ENDPOINT:9000/api/weather"
